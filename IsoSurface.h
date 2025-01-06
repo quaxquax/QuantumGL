78e8e4e1d0e0aab3e33517467d7c0d5a6b16f852
Henning Dekant <henning.dekant@gmail.com>
2025-01-05 19:38:05 -0500

    Added header files used for compilation on OSX.

--- IsoSurface.h
+++ IsoSurface.h
@@ -13,82 +13,80 @@ class ValueEvaluator;
 
 class IsoSurface : public VisualObject
 {
-	struct Critical
-	{
-		vecN3	cube;
-		GLubyte	type;
-	};	
-	
-	number	threshold;
-	
-	DynArray<vecR3> theVertices;
-	DynArray<vecR3> theNormals;
-	DynArray<vecR3> theColors;
-	DynArray<GLint> theIndices;
-	DynArray<vecN3>	theCubes;
-	int		displayListIndex;
-	bool	displayListDirty;
-	
-	ExpressionEvaluator	*dataFunction;
-	GeneralCubicData	*genData;	
-	CubicData<number>	*data;
-	
-	vecR3 interpolate2(int x, int y, int z, int dx, int dy, int dz);
-	vecR3 interpolateEdge(vecN3 pt, int edge);
-
-	vecR3 delta, delta2;
-
-	ValueEvaluator	*thresholdExpr;
-
-	void	Build(number thresh);
-
-	class FieldPrecalculationStage : public CalculationStage
-	{
-	public:
-		IsoSurface *surf;
-		
-					FieldPrecalculationStage();
-	protected:
-		virtual void CalculateStage();
-	};
-	FieldPrecalculationStage precalcStage;
-	
-	bool	isCyclic;
-	number	cycleAt;
-
-	Range	xRange, yRange, zRange;
+    struct Critical
+    {
+        vecN3   cube;
+        GLubyte type;
+    };  
+    
+    number  threshold;
+    
+    DynArray<vecR3> theVertices;
+    DynArray<vecR3> theNormals;
+    DynArray<vecR3> theColors;
+    DynArray<GLint> theIndices;
+    DynArray<vecN3> theCubes;
+    
+    // Modern OpenGL additions
+    GLuint vao;
+    GLuint vboVertices;
+    GLuint vboNormals;
+    GLuint vboColors;
+    GLuint ebo;
+    GLuint shaderProgram;
+    bool buffersInitialized;
+    
+    ExpressionEvaluator  *dataFunction;
+    GeneralCubicData    *genData;   
+    CubicData<number>   *data;
+    
+    vecR3 interpolate2(int x, int y, int z, int dx, int dy, int dz);
+    vecR3 interpolateEdge(vecN3 pt, int edge);
+    vecR3 delta, delta2;
+    ValueEvaluator  *thresholdExpr;
+    void    Build(number thresh);
+    
+    // Add shader initialization methods
+    void initializeShaders();
+    void initializeBuffers();
+    void updateBuffers();
+    
+    class FieldPrecalculationStage : public CalculationStage
+    {
+    public:
+        IsoSurface *surf;
+        
+                    FieldPrecalculationStage();
+    protected:
+        virtual void CalculateStage();
+    };
+    FieldPrecalculationStage precalcStage;
+    
+    bool    isCyclic;
+    number  cycleAt;
+    Range   xRange, yRange, zRange;
+    
 public:
-			IsoSurface();
-	virtual	~IsoSurface();
-			
-	void	SetData(GeneralCubicData *someData);
-	void	SetData(ExpressionEvaluator *function);
-	void	SetThreshold(ValueEvaluator* thresh);
-	void	SetCyclic(number cycle)
-			{ isCyclic = true, cycleAt = cycle; }
-
-	void	Draw(GLfloat camera[3], GLfloat light[3]);
-	
-	void	Build();
-	void	SetGLObjectsDirty();
-
-	void	SubmitToBSP();
-	void	OutputPOV(ostream& out);
-	
-	/*RealVariableWatcher	*GetDataFunctionWatcher()
-			{	return &dataFunctionWatcher;	}*/
-	/*RealVariableWatcher	*GetIsoLevelWatcher()
-			{	return &isoLevelWatcher;	}*/
-
-	CalculationStage	*GetFieldEvaluationStage()
-			{	return &precalcStage;	}
-
-	void	ReevaluateField();
-
-	virtual bool	GetObjectRanges(Range& xr, Range& yr, Range& zr);
-
-	virtual bool	GetRecommendedBSPResolution(vecN3& res);
+            IsoSurface();
+    virtual ~IsoSurface();
+            
+    void    SetData(GeneralCubicData *someData);
+    void    SetData(ExpressionEvaluator *function);
+    void    SetThreshold(ValueEvaluator* thresh);
+    void    SetCyclic(number cycle)
+            { isCyclic = true, cycleAt = cycle; }
+    void    Draw(GLfloat camera[3], GLfloat light[3]);
+    
+    void    Build();
+    void    SetGLObjectsDirty();
+    void    SubmitToBSP();
+    void    OutputPOV(ostream& out);
+    
+    CalculationStage *GetFieldEvaluationStage()
+            { return &precalcStage; }
+    void    ReevaluateField();
+    virtual bool GetObjectRanges(Range& xr, Range& yr, Range& zr);
+    virtual bool GetRecommendedBSPResolution(vecN3& res);
 };
-
 
 #endif
